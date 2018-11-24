3fe7ff82015250d53facc99714fa52a921cfdd15
Tim Russell <tim@jatra.co.uk>
2018-11-24 09:29:27 +0000

    Initial commit

---
+++ app/src/main/java/uk/co/jatra/datetimepicker/MainActivity.kt
@@ -0,0 +1,99 @@
+package uk.co.jatra.datetimepicker
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.SCROLL_STATE_IDLE
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import org.joda.time.LocalDate
+
+fun timeString(count: Int) : String {
+    val hour = if ((count / 2) == 12) 12 else ((count / 2) % 12)
+    val min = if ((count % 2) == 0) "00" else "30"
+    val amOrPm = if (count < 24) "am" else "pm"
+    return "${String.format("%02d", hour)}:$min $amOrPm"
+}
+
+val days = arrayOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+
+fun dateString(count: Int) : String {
+    val date: LocalDate = LocalDate.now().plusDays(count)
+    return "${days[date.dayOfWeek - 1]} ${date.toString("dd MMM yy")}"
+}
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        date.adapter = DateAdapter(this)
+        time.adapter = TimeAdapter(this)
+        val dateLayoutManager = LinearLayoutManager(this)
+        date.layoutManager = dateLayoutManager
+        val timeLayoutManager = LinearLayoutManager(this)
+        time.layoutManager = timeLayoutManager
+        date.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == SCROLL_STATE_IDLE) {
+                    selectedDate.text = dateString(dateLayoutManager.findFirstCompletelyVisibleItemPosition())
+                }
+            }
+        })
+        time.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == SCROLL_STATE_IDLE) {
+                    selectedTime.text = timeString(timeLayoutManager.findFirstCompletelyVisibleItemPosition())
+                }
+            }
+        })
+    }
+
+    class TimeAdapter(val context: Context) : RecyclerView.Adapter<TimeAdapter.TimeViewHolder>() {
+
+        override fun onBindViewHolder(viewHolder: TimeViewHolder, position: Int) {
+            viewHolder.textView.text = timeString(position)
+        }
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): TimeViewHolder {
+            val view = LayoutInflater.from(context).inflate(R.layout.date_or_time, p0, false)
+            return TimeViewHolder(view)
+        }
+
+        override fun getItemCount() = 48
+
+        class TimeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val textView = itemView.findViewById<TextView>(R.id.textView)
+        }
+
+
+    }
+    class DateAdapter(val context: Context) : RecyclerView.Adapter<DateAdapter.DateViewHolder>() {
+
+
+        override fun onBindViewHolder(viewHolder: DateAdapter.DateViewHolder, position: Int) {
+            viewHolder.textView.text = dateString(position)
+        }
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): DateAdapter.DateViewHolder {
+            val view = LayoutInflater.from(context).inflate(R.layout.date_or_time, p0, false)
+            return DateViewHolder(view)
+        }
+
+        override fun getItemCount() =  10_000
+
+        class DateViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val textView = itemView.findViewById<TextView>(R.id.textView)
+        }
+
+
+    }
+}
